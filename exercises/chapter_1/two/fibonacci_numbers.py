e4fd37465664cd0114139754b91b494cedbf1e6d
Jonathan Harker <jonathan.harker@complyadvantage.com>
2022-01-17 16:09:49 +0000

    refactor: print F(1)...f(n)

--- exercises/chapter_1/two/fibonacci_numbers.py
+++ exercises/chapter_1/two/fibonacci_numbers.py
@@ -1,10 +1,14 @@
 def print_fibanacci_sequence(limit: int):
     fib_n_minus_2 = 0
-    fib_n_minus_1 = 1
-    fib_idx = 2
+    fib_n_minus_1 = 0
+    fib_idx = 1
 
     while fib_idx <= limit:
         fib_n = fib_n_minus_2 + fib_n_minus_1
+
+        if fib_n == 0:
+            fib_n = 1
+
         print(fib_idx, float(fib_n))
         fib_n_minus_2, fib_n_minus_1 = fib_n_minus_1, fib_n
         fib_idx += 1
